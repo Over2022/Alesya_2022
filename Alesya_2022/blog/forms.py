9b3ad6eef3e1fe8f397abd01f7ea39e53fa11ae0
Алеська <1912977@mail.ru>
2022-09-29 16:31:44 +0300

    blog_category

--- Alesya_2022/blog/forms.py
+++ Alesya_2022/blog/forms.py
@@ -3,14 +3,14 @@ from django import forms
 
 class CommentForm(forms.Form):
     author = forms.CharField(max_length=60,
-                             widdget=forms.TextInput(attrs={
+                             widget=forms.TextInput(attrs={
                                  'class': 'form-control',
                                  'placeholder': 'Ваше имя'
                              })
                              )
 
     body=forms.CharField(
-                             widdget=forms.Textarea(attrs={
+                             widget=forms.Textarea(attrs={
                                  'class': 'form-control',
                                  'placeholder': 'Ваш комментарий'
                              })
